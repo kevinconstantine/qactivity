0502589144fdf1a73f4cdfbb06592b49c9f3b626
Michael Murray <mmurray@qumulo.com>
2016-04-05 16:30:06 -0700

    backed out flask stuff for the moment

---
+++ qactivity_tables.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from sqlalchemy import Column, Integer, BigInteger, String, Interval
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Iops(Base):
+    __tablename__ = 'iops'
+    id = Column(Integer, primary_key=True)
+    ts = Column(Interval, index=True)
+    ip = Column(String, index=True)
+    path = Column(String, index=True)
+    iops = Column(String)
+
+    def __repr__(self):
+        return '<Iops %r>' % str(self.id)
+
+class Capacity(Base):
+    __tablename__ = 'capacity'
+    id = Column(Integer, primary_key=True)
+    ts = Column(Interval, index=True)
+    path = Column(String, index=True)
+    size = Column(BigInteger)
+
+    def __repr__(self):
+        return '<Capacity %r>' % str(self.id)
+
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+engine = create_engine('sqlite:///qactivity.db')
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
